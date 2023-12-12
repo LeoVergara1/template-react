7ff700cd7108e2556e20370df3d64f7277a9de19
Brandon Vergara <brandon@makingdevs.com>
2023-12-11 19:28:53 -0600

    [PNP-1933] add router

---
+++ src/views/About.jsx
@@ -0,0 +1,13 @@
+// About.jsx
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Acerca de</h1>
+      <p>Esta es la página que describe nuestra aplicación.</p>
+    </div>
+  );
+}
+
+export default About;

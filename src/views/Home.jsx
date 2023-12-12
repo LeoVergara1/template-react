e2cb24ac9d8227304a864b51240106bfab6e6c1d
Brandon Vergara <brandon@makingdevs.com>
2023-12-11 21:13:26 -0600

    [main] add project to theme

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,6 +4,21 @@ function Home() {
     <div>
       <h1>Bienvenido a la página de inicio</h1>
       <p>Esta es la página de inicio de nuestra aplicación.</p>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <button className="btn btn-primary">Button</button>
+      <div className="flex">
+        <div>hola</div>
+        <div>hola</div>
+      </div>
     </div>
   );
 }
